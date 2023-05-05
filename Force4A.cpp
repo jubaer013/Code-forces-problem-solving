48980ef782bdb1edee5df35e7301207477c385b4
Robiul Islam Jubaer <113488476+jubaer013@users.noreply.github.com>
2023-05-06 00:49:40 +0600

    Add files via upload

---
+++ Force4A.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    if(a>2 && a%2 == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
